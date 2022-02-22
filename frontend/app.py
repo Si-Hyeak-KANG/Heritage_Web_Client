17014bd789026ba589f872c1b7d7e583f3495c2e
Daun jeong <qkrtjrfo1@gmail.com>
2022-02-22 12:32:43 +0900

    (index) Ajax 연결 완료, 그러나 boardsList 내 tbody id="listBox"로 붙이면 세로로 붙는 현상

--- frontend/app.py
+++ frontend/app.py
@@ -13,27 +13,28 @@ def home():
    return render_template('index.html')
 
 @app.route("/heritage", methods=["POST"])
-def web_mars_post():
+def web_heritage_post():
     ID_receive = request.form['ID_give']
     PW_receive = request.form['PW_give']
     Sector_receive = request.form['Sector_give']
     Title_receive = request.form['Title_give']
     Comment_receive = request.form['Comment_give']
-    # likeCount_receive  = request.form['likeCount_give']
+
     doc = {
         'ID': ID_receive,
         'PW' : PW_receive,
         'Sector' : Sector_receive,
         'Title' : Title_receive,
         'Comment' : Comment_receive
-        # 'likeCount' : likeCount_receive
+
     }
     db.heritage.insert_one(doc)
     return jsonify({'msg': '글쓰기 완료!'})
 
-@app.route("/mars", methods=["GET"])
-def web_mars_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+@app.route("/heritage", methods=["GET"])
+def web_heritage_get():
+    allList = list(db.heritage.find({}, {'_id': False}))
+    return jsonify({'lists': allList})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
